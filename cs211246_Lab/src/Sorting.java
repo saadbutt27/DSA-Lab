fe2985b69eb3bbf22c7b2771b4fd0a0ccd574c18
Saad Nisar Butt <83548173+saadbutt27@users.noreply.github.com>
2022-11-15 08:33:47 +0500

    insertion sort added

--- cs211246_Lab/src/Sorting.java
+++ cs211246_Lab/src/Sorting.java
@@ -53,4 +53,27 @@ public class Sorting {
         }
         return arr;
     }
+    
+    public int[] insertionSort(int arr[]) {
+        for (int i=1; i<arr.length; i++) {
+            int temp = arr[i];
+            int j = i-1;
+            while (j >= 0 && arr[j] > temp) {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = temp;
+        }
+        return arr;
+    }
+    public int[] insertionSortForLoop(int arr[]) {
+        for (int i=1; i<arr.length; i++) {
+            int temp = arr[i];
+            for (int j=i-1; j >= 0 && arr[j] > temp; j--) {
+                arr[j+1] = temp;
+            }
+        }
+        return arr;
+    }
+    
 }
