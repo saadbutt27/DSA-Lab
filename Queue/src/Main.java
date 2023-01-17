1c7144927dc97003a5f12075a15ea9492e09337a
Saad Nisar Butt <sb03750@gmail.com>
2023-01-17 18:51:56 +0500

    priority queue updated

--- Queue/src/Main.java
+++ Queue/src/Main.java
@@ -15,6 +15,18 @@ public class Main {
         lq.dequeue();
 
         System.out.println(lq.peek().data);
+        System.out.println();
+
+        System.out.println("Priority Queue");
+        PriorityQueue pq = new PriorityQueue();
+        pq.enqueue(31,3);
+        pq.enqueue(25,4);
+        pq.enqueue(54,2);
+        System.out.println(pq.peek().data);
+        pq.enqueue(12,1);
+        pq.enqueue(62,6);
+        pq.enqueue(46,5);
+        System.out.println(pq.peek().data);
 
     }
 }
